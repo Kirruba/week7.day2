ff4a5581db58eed2a77153f8d250e37cc56e6653
Kirruba <112795574+Kirruba@users.noreply.github.com>
2022-10-22 13:36:09 +0530

    Add files via upload

--- salesforcerunner/runner.java
+++ salesforcerunner/runner.java
@@ -1,1 +1,9 @@
+package week7.day2.salesforcerunner;
 
+import io.cucumber.testng.CucumberOptions;
+import week7.day2.salesforcesteps.BaseClass;
+
+@CucumberOptions(features = "src/test/java/week7/day2/salesforcefeature", glue = "week7.day2.salesforcesteps", monochrome = true, publish = true, tags = "@regression or @functional or @smoke and @sanity")
+public class runner extends BaseClass {
+
+}
